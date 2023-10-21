6268e5bafa5c3bf4afedb1261b11fe6dcf1b550e
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-10-21 17:52:40 -0300

    Update participante.h

--- Trabalho 01/participante.h
+++ Trabalho 01/participante.h
@@ -2,4 +2,8 @@ typedef struct participantes Participantes;
 
 void insert(Participantes** lista, char *nome_artista, char *nome_personagem, char *descricao);
 void imprimir(Participantes* lista);
-Participante* encontrar_participante(Participante *no, char *nome_artista);
+Participantes* encontrar_participante(Participante *no, char *nome_artista);
+Participantes* criar(char *nome_artista, char *nome_personagem, char *descricao);
+Participantes* sort(Participantes* head);
+Participantes* _Creater();
+Participantes* _Init();
