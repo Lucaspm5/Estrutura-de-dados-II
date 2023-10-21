035329601a9505d7fdf6f6fea1adb241fd46686e
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-10-21 17:57:29 -0300

    Update temporadas.h

--- Trabalho 01/temporadas.h
+++ Trabalho 01/temporadas.h
@@ -1,11 +1,4 @@
-#include "participante.h"
-
-typedef struct Temporada {
-    int numero, qtd_episodios, ano;
-    char titulo[50];
-    Participante* participantes;
-    struct Temporada *l, *r;
-} Temporada;
+typedef struct Temporada temp;
 
 Temporada* inserir_temporada(Temporada* raiz, int numero, char* titulo, int qtd_episodios, int ano);
 void imprimir_temporadas(Temporada* raiz);
