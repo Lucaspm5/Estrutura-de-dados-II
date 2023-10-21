694138c92baf763827cb3755c0a85b794640ffa5
Fabrício Liedson <111002008+Liedsonfsa@users.noreply.github.com>
2023-10-21 20:02:32 -0300

    Update series.h

--- Trabalho 01/series.h
+++ Trabalho 01/series.h
@@ -1,11 +1,4 @@
-#include "temporadas.h"
+typedef struct series Series;
 
-typedef struct Serie {
-    int codigo, num_temporadas;
-    char titulo[50];
-    Temporada* temporadas;
-    struct Serie *esq, *dir;
-} Serie;
-
-Serie* inserir_serie(Serie* raiz, int codigo, char* titulo, int num_temporadas);
-void imprimir_series(Serie* raiz);
+void inserir_serie(Series** root, Series* node);
+void imprimir_series(Series* root);
