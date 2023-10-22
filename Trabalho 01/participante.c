150ac8e80a938988bc8adf3b934ab5fa1e3c7798
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-10-22 16:43:40 -0300

    Update participante.c

--- Trabalho 01/participante.c
+++ Trabalho 01/participante.c
@@ -13,51 +13,38 @@ struct participantes {
 };
 //--------------------------------------------------------------------
 Participantes* criar(char *nome_artista, char *nome_personagem, char *descricao) {
-	Participantes* novo = ALOCAR(Participantes);
-	strcpy(novo->nome_artista, nome_artista);
- 	strcpy(novo->nome_personagem, nome_personagem);
- 	strcpy(novo->descricao, descricao);
- 	novo->next = NULL;
-	return novo;
+	Participantes* no = ALOCAR(Participantes);
+	strcpy(no->nome_artista, nome_artista);
+ 	strcpy(no->nome_personagem, nome_personagem);
+ 	strcpy(no->descricao, descricao);
+ 	no->next = NULL;
+	return no;
 }
 //--------------------------------------------------------------------
-void insertparticipantes(Participantes** lista, char *nome_artista, char *nome_personagem, char *descricao) {
-	if (*lista == NULL) {
-		Participantes* novo = criar(nome_artista, nome_personagem, descricao);
-		(*lista) = novo;
-	} else {
-		insertparticipantes(&(*lista)->next, nome_artista, nome_personagem, descricao);
-	}
+void insertAndSort(Participantes **head, char *nome_artista, char *nome_personagem, char *descricao) {
+    Participantes* novo = criar(nome_artista, nome_personagem, descricao);
+    if (!(*head)) {
+        *head = novo;
+    } else {
+        Participantes* copy = *head;
+        Participantes* aux = NULL;
+        while (copy && strcmp(copy->nome_artista, nome_artista) < 0) {
+            aux = copy;
+            copy = copy->next;
+        }
+        if (!aux) {
+            novo->next = *head;
+            *head = novo;
+        } else {
+            aux->next = novo;
+            novo->next = copy;
+        }
+    }
 }
-//--------------------------------------------------------------------
-int _Validparticipantes(Participantes *no) { return (no) ? 1 : 0; }
 //--------------------------------------------------------------------
 void imprimir_participantes(Participantes* lista) {
-	if (!_Validparticipantes(lista)) {
-		printf("Ocorreu um erro, sem participantes cadastrados\n");
-	} else {
-		if (lista) {
-			printf("Ator: %s\nPapel: %s\nDescricao: %s\n", lista->nome_artista, lista->nome_personagem, lista->descricao);
-			imprimir_participantes(lista->next);
-		}
+	if (lista) {
+		printf("Ator: %s\nPapel: %s\nDescricao: %s\n", lista->nome_artista, lista->nome_personagem, lista->descricao);
+		imprimir_participantes(lista->next);
 	}
-}
-//--------------------------------------------------------------------
-Participantes* sort(Participantes* head) {
-	if (!head || !head->next) return head;
-
-    head->next = sort(head->next);
-
-    Participantes* next = head->next;
-    if (strcmp(head->nome_artista, next->nome_artista) <= 0)
-        return head;
-
-    Participantes* node = next;
-    while (node->next && strcmp(node->next->nome_artista, head->nome_artista) < 0)
-        node = node->next;
-
-    head->next = node->next;
-    node->next = head;
-
-    return next;
 }
